869978e58f97fcbd21a12f4da17b348d39d227c7
Suzuki Kyosuke <suzuki-kyosuke@in.kayac.com>
2017-05-22 18:22:37 +0900

    indent追加

--- link.sh
+++ link.sh
@@ -4,4 +4,5 @@ DOTFILES=$(cd $(dirname $0); pwd)
 
 ln -sf ${DOTFILES}/.bash_profile ${HOME}/
 ln -sf ${DOTFILES}/.config/nvim/init.vim ${HOME}/.config/nvim/
+ln -sf ${DOTFILES}/.config/nvim/indent ${HOME}/.config/nvim/
 ln -sf ${DOTFILES}/.config/git/git-completion.bash ${HOME}/.config/git/
